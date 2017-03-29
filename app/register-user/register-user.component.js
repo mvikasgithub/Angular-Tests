fc1b5c382be5a78149c9b509b17c9bb13aac0c74
mvikasgithub <vikasm@gmail.com>
2017-03-29 13:57:41 +0530

    Registering user succesfully and adding to DB

--- app/register-user/register-user.component.js
+++ app/register-user/register-user.component.js
@@ -2,26 +2,41 @@
 
 angular.
     module('registerUser').
-        component('registerUser', {
-            templateUrl: 'register-user/register-user.template.html',
-            controller: function RegisterUserController() {
+    component('registerUser', {
+        templateUrl: 'register-user/register-user.template.html',
+        controller: function RegisterUserController($http, $q) {
 
-                var self = this;
-                                
-                
-                self.register = function register(user){
-                    console.log(self.user);
-                    console.log(self.confirmPassword);
+            var self = this;
+            self.user = new Object();
+            self.user.role = 'student';
 
-                    if(self.user.password != self.confirmPassword){
-                        self.message = "Passwords do not match. Re-enter password";
-                        self.confirmPassword = "";
-                        self.user.password = "";
-                    }
-                    else
-                        self.message = "Registration Successful";
-                        
+
+            self.register = function register(user) {
+                //console.log(self.user);
+                //console.log(self.confirmPassword);
+
+                var deferred = $q.deferred;
+
+                if (self.user.password != self.confirmPassword) {
+                    self.message = "Passwords do not match. Re-enter password";
+                    self.confirmPassword = "";
+                    self.user.password = "";
                 }
+                else
+                    self.message = "Registration Successful";
+
+                $http.post('http://localhost:8080/onlinecollaboration/user/receive', self.user).then(
+                    function (response) {
+                        deferred.resolve(response.data);
+                        console.log(response.data);
+                    },
+                    function (error) {
+                        console.log(error);
+                        deferred.reject(error);
+                    }
+                );
 
             }
-        });
+
+        }
+    });
